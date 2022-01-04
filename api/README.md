4628d6584a636b84c193d3044b2fb0be1484f039
David Álvarez Pons <david_alvarezpons@hotmail.com>
2022-01-04 23:08:22 +0100

    Add actions api

---
+++ api/README.md
@@ -0,0 +1,4 @@
+```
+curl -d '{"action":"forward"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:80/action
+curl http://127.0.0.1/action
+```
---
+++ api/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var action = 'none';
+
+var VALID_ACTIONS = {
+    'none': true,
+    'forward': true,
+    'backward': true,
+    'right': true,
+    'left': true,
+};
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/action', function (req, res) {
+    console.log("Hande GET on route: " + req.path)
+    res.send(action);
+});
+
+app.post('/action', function (req, res) {
+    console.log("Hande POST on route: " + req.path)
+    if (req.body.action in VALID_ACTIONS) {
+        action = req.body.action;
+    } else {
+        console.log('Invalid action: ', req.body);
+    }
+    res.send();
+});
+
+app.listen(80, function () {
+    console.log('node-http Server listening on port 80');
+});
